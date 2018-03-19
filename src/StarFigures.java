5d7a7d83d9df7ab7356c4982ec35116f52b4a109
Destiny Goubeaux <dgoubeaux1@live.maryville.edu>
2018-03-18 19:41:41 -0500

    Answered 1-5

--- src/StarFigures.java
+++ src/StarFigures.java
@@ -8,9 +8,39 @@
 
 
 public class StarFigures {
-
 	public static void main(String[] args) {
-
+		Drawlines();
+		DrawX();
+		System.out.println();
+		System.out.println();
+		Drawlines();
+		DrawX();
+		Drawlines();
+		System.out.println();
+		System.out.println();
+		DrawAsterick();
+		Drawlines();
+		DrawX();
+	}
+	
+	
+	public static void Drawlines() {
+		System.out.println("*****");
+		System.out.println("*****");
+	}
+	
+	public static void DrawX() {
+		System.out.println(" * *");
+		System.out.println("  *");
+		System.out.println(" * *");
 	}
 
-}
+	public static void DrawAsterick() {
+		System.out.println("  *");
+		System.out.println("  *");
+		System.out.println("  *");
+		}
+		
+	}
+
+
--- src/Tricky.java
+++ src/Tricky.java
@@ -5,13 +5,34 @@
 */
 
 // 1. Your predicted output 
- 
 
-public class Tricky {
+/* Done with main. 
 
-}
+This is message1.
+This is message2.
+
+Done with message2. */
+
+ public class Tricky {
+	    public static void main(String[] args) {
+	        message1();
+	        message2();
+	        System.out.println("Done with main.");
+	    }
+	    
+	    public static void message1() {
+	        System.out.println("This is message1.");
+	    }
+	    
+	    public static void message2() {
+	        System.out.println("This is message2.");
+	        message1();
+	        System.out.println("Done with message 2.");
+	    }
+	}
 
 // 3. Were you correct? Explain any differences
  
- 
+ /* My prediction was not right. I had my ordering completely wrong.
+  	Now that i see the answer i see what i did wrong. */
 
